772fb967d16dd902c10e17e002003a47f006449c
Anil Seervi <61609033+AnilSeervi@users.noreply.github.com>
2022-01-08 23:05:18 +0530

    🎨 Add npm link and 🔍️ open-graph details

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -8,7 +8,7 @@ const SEO = () => {
         content="width=device-width,initial-scale=1,minimum-scale=1,user-scalable=yes"
       />
       <link rel="shortcut icon" href="favicon.svg" type="image/svg" />
-      <title>Anil Seervi | Front-End Developer</title>
+      <title>ANIL SEERVI | Front-End Developer</title>
       <meta
         name="description"
         content="Anil Seervi is a Front-End Developer from India. This website contains social links of his accounts in other platform."
@@ -16,6 +16,27 @@ const SEO = () => {
       <meta name="robots" content="index,follow" />
       <meta name="theme-color" content="#a033ff" />
       <meta name="author" content="Anil Seervi" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content="@linASeervi" />
+      <meta name="twitter:creator:id" content="2496313362" />
+      <meta name="twitter:site:id" content="2496313362" />
+
+      <meta property="og:description" content="Front-End Developer" />
+      <meta
+        property="og:image"
+        content="https://anil.pages.dev/assets/og-thumb.png"
+      />
+      <meta property="og:image:alt" content="Anil Seervi" />
+      <meta property="og:image:height" content="1080" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1920" />
+      <meta
+        property="og:site_name"
+        content="Anil Seervi | Front-End Developer"
+      />
+      <meta property="og:title" content="Anil Seervi" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://anil.pages.dev/" />
     </Head>
   );
 };
